a8fd04d0febe96bd9df7dd25502ecc522204154f
AlexKVal <alexkval@gmail.com>
2015-05-19 14:25:26 +0300

    Use react-mount

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import {Button, ListGroup, ListGroupItem} from 'react-bootstrap';
+/* eslint react/no-multi-comp: 0 */
 
-var App = React.createClass({
-  displayName: 'App',
+import React from 'react';
+import mount from 'react-mount';
+import { Button } from 'react-bootstrap';
+
+const CustomComponent = React.createClass({
+  displayName: 'CustomComponent',
 
   render: function () {
-    var items = [
-      <ListGroupItem>Item 1</ListGroupItem>
-    ];
     return (
       <div>
-        <Button bsStyle='primary'>This is the Button 2</Button>
-        <ListGroup>
-          {items}
-        </ListGroup>
+        <Button bsStyle='primary'>{this.props.children}</Button>
+        <Button bsStyle='info'>{this.props.children}</Button>
       </div>
     );
   }
-
 });
 
-React.render(<App />, document.getElementById('app'));
+const Simple = React.createClass({
+  displayName: 'Simple',
+
+  render: function () {
+    return (
+      <p>Simple Text</p>
+    );
+  }
+});
+
+const App = React.createClass({
+  displayName: 'App',
+
+  render: function () {
+    return (
+      <div>
+        <Simple></Simple>
+        <CustomComponent>This is the Button</CustomComponent>
+        <custom-component>Inner for CustomComponent</custom-component>
+      </div>
+    );
+  }
+});
+
+mount({
+  App,
+  'simple': Simple,
+  'custom-component': CustomComponent
+});
+
+// React.render(<App />, document.getElementById('app'));
