64f67f66e8d620efd5e015eb2656343e71b411b4
AlexKVal <alexkval@gmail.com>
2015-05-20 22:00:19 +0300

    Update dependencies. Check ButtonInput.

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,31 @@
 /* eslint react/no-multi-comp: 0 */
-
 import React from 'react';
-import mount from 'react-mount';
-import { Button } from 'react-bootstrap';
 
-const CustomComponent = React.createClass({
-  displayName: 'CustomComponent',
-
-  render: function () {
-    return (
-      <div>
-        <Button bsStyle='primary'>{this.props.children}</Button>
-        <Button bsStyle='info'>{this.props.children}</Button>
-      </div>
-    );
-  }
-});
-
-const Simple = React.createClass({
-  displayName: 'Simple',
-
-  render: function () {
-    return (
-      <p>Simple Text</p>
-    );
-  }
-});
+import {
+  // Input,
+  ButtonInput
+} from 'react-bootstrap';
 
 const App = React.createClass({
   displayName: 'App',
 
   render: function () {
     return (
-      <div>
-        <Simple></Simple>
-        <CustomComponent>This is the Button</CustomComponent>
-        <custom-component>Inner for CustomComponent</custom-component>
-      </div>
+      <form>
+        <ButtonInput
+          bsSize="small">Child Text</ButtonInput>
+        <ButtonInput
+          type="reset"
+          bsStyle="primary" />
+        <ButtonInput
+          type="submit"
+          value="Submit Your Input"
+          bsStyle="info"
+          bsSize="large"
+          disabled={false} />
+      </form>
     );
   }
 });
 
-mount({
-  App,
-  'simple': Simple,
-  'custom-component': CustomComponent
-});
-
-// React.render(<App />, document.getElementById('app'));
+React.render(<App />, document.getElementById('app'));
